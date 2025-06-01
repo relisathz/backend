6f47a97d41e679e833beb571984d1dcdca063331
relisathz <dula98sathsarani@gmail.com>
2025-06-01 15:41:50 +0530

    viedeo 7

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 import express from 'express';
 // const express = require('express');
 const app = express();
-import { getProducts,createProduct } from './Application/product.js';
+import { getProducts,createProduct,getProduct,deleteProduct,updateProduct} from './Application/product.js';
+// import { getProduct4 } from './api/product.js';
 
 app.use(express.json()); // For parsing JSON requests
 
 // const sendHello = (req, res) => res.send('Hello world!')
-
 // app.get('/hello', sendHello);
+//product routering....it's in route product.js
 
-app.get('/products',getProducts)
-app.post('/products',createProduct)
+ const productRouter=express.Router()
+ productRouter.route('/').get(getProducts).post(createProduct)
+//router define
+productRouter.route('/:id').get(getProduct).delete(deleteProduct).patch(updateProduct)
+//import { productRouter } from './Route/product.js';
+app.use('/products',productRouter) 
 
+// app.get('/products',getProducts)
+// app.post('/products',createProduct)
 
+//app.get('/product/:id',getProduct)
+
+//delete
+//app.delete('/product/:id',deleteProduct)
+
+//app.patch('/product/:id',updateProduct)
+
+//  app.delete('/product/:id',deleteProducts4)
 
 app.listen(8000, () => console.log(`Server running on port ${8000}`));
